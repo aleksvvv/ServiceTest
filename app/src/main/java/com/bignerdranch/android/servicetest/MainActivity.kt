09c3c245216c6168b463af3ac5deaec421a14660
aleksvvv <adm-shop-001@yandex.ru>
2023-03-27 11:34:56 +0300

    Intent

--- app/src/main/java/com/bignerdranch/android/servicetest/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/servicetest/MainActivity.kt
@@ -27,6 +27,9 @@ class MainActivity : AppCompatActivity() {
         binding.foregroundService.setOnClickListener {
             ContextCompat.startForegroundService(this,MyForegroundService.newIntent(this))
         }
+        binding.intentService.setOnClickListener {
+            startService(MyIntentService.newIntent(this))
+        }
     }
 
     private fun showNotification() {
